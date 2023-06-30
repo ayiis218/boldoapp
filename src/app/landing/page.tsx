1bcc56e90f674585169cece12c1cf6e887517bfe
unknown <ayi.solahudin.konsultan@work.bri.co.id>
2023-06-30 16:50:19 +0700

    first commit

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,9 @@
+export default function Landing() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <h1>Hello word</h1>
+      </div>
+    </main>
+  )
+}
